568d6bb2b55f97307836949fcf7ce7ef9950b4b9
Viral_1704 <viralvaghasiya8@gmail.com>
2025-11-05 21:03:56 +0530

    created database.

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -3,12 +3,20 @@ from flask import Flask
 from .routes.auth import auth
 from .routes.main import main
 
+from .commands import create_tables
+
+from .extensions import db
+
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
 
     app.config.from_pyfile(config_file)
 
+    db.init_app(app)
+
     app.register_blueprint(main)
     app.register_blueprint(auth)
+
+    app.cli.add_command(create_tables)
 
     return app
---
+++ dashboard/commands.py
@@ -0,0 +1,12 @@
+import click
+
+from flask.cli import with_appcontext
+
+from .extensions import db
+from .models import Customer, Product, Order
+
+
+@click.command('create_tables')
+@with_appcontext
+def create_tables():
+    db.create_all()
