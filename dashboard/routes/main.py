d75b8adfe43822212fd9a33556508dc31a5eb92e
Viral_1704 <viralvaghasiya8@gmail.com>
2025-11-14 12:21:25 +0530

    now we completed earning overview task

--- dashboard/routes/main.py
+++ dashboard/routes/main.py
@@ -14,13 +14,13 @@ def index():
     orders_today = Order.query.filter(Order.date > beginning_of_day).count()
 
 
-    monthly_earings = Order.get_monthly_earnings()
+    monthly_earnings = Order.get_monthly_earnings()
 
 
     yearly_earnings = 0
     this_year = datetime.today().year
 
-    for month in monthly_earings:
+    for month in monthly_earnings:
         if month[0] == this_year:
             yearly_earnings += month[2]
 
@@ -44,19 +44,31 @@ def index():
 
         total_goal += product.monthly_goal
 
-    earnings_this_month = monthly_earings[-1][2]
+    earnings_this_month = monthly_earnings[-1][2]
     monthly_goal_percentage = (earnings_this_month / total_goal) * 100
 
     if monthly_goal_percentage > 100:
         monthly_goal_percentage = 100
 
 
+    monthly_earnings_array = []
+    for earnings in monthly_earnings[-12:]:
+        monthly_earnings_array.append(earnings[2])
+
+
+    this_month = datetime.today().month
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    months = months[this_month:] + months[:this_month]
+
+
     context = {
         'orders_today' : orders_today,
-        'earnings_this_month' : monthly_earings[-1][2],
+        'earnings_this_month' : monthly_earnings[-1][2],
         'yearly_earnings' : yearly_earnings,
         'product_goals' : product_goals,
-        'monthly_goal_percentage' : monthly_goal_percentage
+        'monthly_goal_percentage' : monthly_goal_percentage,
+        'monthly_earnings_array' : monthly_earnings_array,
+        'months' : months
     }
 
     return render_template('index.html', **context)
